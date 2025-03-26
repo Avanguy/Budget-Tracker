f6d72cf837e56030f414cf40da6837bd08e64ba8
Avanguy <nguyenvalbert@gmail.com>
2025-03-26 13:29:42 +0000

    Initial commit for Budget tracker full stack project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 class="text-3xl text-center font-bold underline text-fuchsia-400">
+          Hello world!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
